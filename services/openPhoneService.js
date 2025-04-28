66103a9d578d215a4c48af63be756f95a1870aa2
Doughty94 <doughtyd94@gmail.com>
2025-04-27 22:07:44 -0400

    Add Procfile for Railway startup

--- services/openPhoneService.js
+++ services/openPhoneService.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const https = require('https');
 
 const sendTextMessage = async (toPhoneNumber, messageBody) => {
   try {
@@ -12,7 +13,10 @@ const sendTextMessage = async (toPhoneNumber, messageBody) => {
         headers: {
           Authorization: `Bearer ${process.env.OPENPHONE_API_KEY}`,
           'Content-Type': 'application/json'
-        }
+        },
+        httpsAgent: new https.Agent({  
+          rejectUnauthorized: false  // Ignore SSL cert issues
+        })
       }
     );
     return response.data;
